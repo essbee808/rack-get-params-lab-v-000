702de09aefbd057a51af64783244a4327c70b642
Sheena Bonilla <sheena.myb@gmail.com>
2019-02-23 23:44:14 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -16,7 +16,7 @@ class Application
       resp.write handle_search(search_term)
     elsif req.path.match(/cart/)
       @@cart.each do |el|
-        
+        resp.write "#{el}\n"
       end
     else
       resp.write "Path Not Found"
