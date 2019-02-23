62038e70898a14332fc86f5f9bbf90725a3f2816
Sheena Bonilla <sheena.myb@gmail.com>
2019-02-23 23:50:13 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -18,6 +18,7 @@ class Application
       if !@@cart.empty?
         @@cart.each do |el|
           resp.write "#{el}\n"
+        end
       else
         resp.write "Your cart is empty."
       end
